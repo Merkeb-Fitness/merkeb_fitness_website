9624dee2a11d82f36005da8a639906b1b2d61faf
mikiyasgirma <mikiyasgirma43@gmail.com>
2023-01-31 05:56:48 +0300

    feat: mobile responsive undercon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export interface Props {
 	name: string,
 	image: string,
@@ -11,32 +13,32 @@ export interface Props {
 
 const Card = ({name, image, alt, protien, fat, carbs, calories, price} : Props) => {
 	return (
-		<div className="max-w-sm w-96 h-[349px] rounded-lg overflow-hidden shadow-lg">
-	<div className="w-full h-[268px] relative">
-		<img className="object-cover relative" src={image} alt={alt} />
-		<div className="absolute bottom-6 left-0 space-y-1">
-			<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${protien}"g P"`}</div>
-			<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${carbs}"g C"`}</div>
-			<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${fat}"g F"`}</div>
-			<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${calories}"g Kcl"`}</div>
-		</div>
-	</div>
-	<div className="px-6 pt-4 pb-2 flex justify-between">
-		<div>
-			<div className="font-questrial font-normal text-xl">{name}</div>
-			<div className="flex space-x-4">
-				<button className="bg-accent px-4 py-1 text-center rounded-md font-medium text-xs">Popular</button>
-				{calories > 300 &&
-				<button className="bg-[#00D8B1] px-4 py-1 text-center rounded-md font-medium text-xs">High Calori</button>
-				}
-				{calories < 150 &&
-				<button className="bg-[#FF7A13] px-4 py-1 text-center rounded-md font-medium text-xs">Low Calori</button>
-				}
+		<div className="max-w-sm md:w-96 md:h-[349px] w-[388px] rounded-lg overflow-hidden shadow-lg">
+			<div className="w-full h-[268px] relative">
+				<img className="object-cover relative" src={image} alt={alt} />
+				<div className="absolute bottom-6 left-0 space-y-1">
+					<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${protien}"g P"`}</div>
+					<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${carbs}"g C"`}</div>
+					<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${fat}"g F"`}</div>
+					<div className="bg-white rounded-tr-md rounded-br-md w-20 h-8 font-Rob font-medium text-sm leading-4 text-center py-2">{`${calories}"g Kcl"`}</div>
+				</div>
+			</div>
+			<div className="px-6 pt-4 pb-2 flex justify-between">
+				<div>
+						<div className="font-questrial font-normal text-xl">{name}</div>
+						<div className="flex space-x-4">
+							<button className="bg-accent px-4 py-1 text-center rounded-md font-medium text-xs">Popular</button>
+							{calories > 300 &&
+							<button className="bg-[#00D8B1] px-4 py-1 text-center rounded-md font-medium text-xs">High Calori</button>
+							}
+							{calories < 150 &&
+							<button className="bg-[#FF7A13] px-4 py-1 text-center rounded-md font-medium text-xs">Low Calori</button>
+							}
+						</div>
+				</div>
+				<div className="font-Rob font-medium text-xl">{price}</div>
 			</div>
 		</div>
-		<div className="font-Rob font-medium text-xl">{price}</div>
-	</div>
-</div>
 	)
 }
 export default Card;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+
+import { useEffect, useState } from "react";
+// export interface Props {}
+// import "@fontsource/roboto";
+
+
+const Navbar = () => {
+    const [windowWidth, setWindowWidth] = useState<Number>();
+    console.log("screen width", windowWidth);
+
+    useEffect(()=>{
+        setWindowWidth(window.innerWidth);
+    })
+
+    return (
+        <>
+           { windowWidth &&
+           windowWidth >= 768 ? (
+                <nav className="pl-[100px] pr-[162px] h-full flex items-center bg-primary shadow-2xl shadow-gray-200 ">
+                    <ul className="flex justify-between w-full ">
+                        <li>
+                            <a href="/"><img src="/assets/merkeb-fitness-logo.svg" alt="Merkeb Fitness Logo" /></a>
+                        </li>
+                    <li className="flex space-x-[25px] font-Rob font-medium text-base">
+                        <a href="/menu">Menu</a>
+                        <a href="/#">Contact</a>
+                    </li>
+                    </ul>
+                </nav>
+            ) : (
+                <nav className="flex justify-between p-4">
+                    <div>
+                        <img src="/assets/merkeb-fitness-logo-mobile.png" />
+                    </div>
+                    <div>
+                        <img src="/assets/drawer-icon.svg" />
+                    </div>
+                </nav>
+            )
+            }
+        </>
+    )
+}
+export default Navbar;
